9dd23641480b73f15e65b256d1f00a1997dd184d
Victoria <piklad1996@gmail.com>
2023-06-12 13:03:28 -0500

    js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,8 @@
 // Selects the shop div in the HTML file
 let shop = document.getElementById("shop");
 
-// Holds data about each product card
-let shopItemsData = [
-        {
-            id: "oneID",
-            name:"Casual Shirt",
-            price: 45,
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing" ,
-            img: "images/img-1.jpg"
-        },
-        {   id: "twoID",
-            name:"Office Shirt",
-            price: 100,
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing" ,
-            img: "images/img-2.jpg"
-            },
-        {
-            id: "threeID",
-            name:"T-Shirt",
-            price: 25,
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing" ,
-            img: "images/img-3.jpg"
-        },
-        {
-            id: "fourID",
-            name:"Men's Suit",
-            price: 300,
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing" ,
-            img: "images/img-4.jpg" 
-        }
-    ];
+
+
 
 // Retrieves data stored, or if nothing has been stored, sets up an empty array
 let basket = JSON.parse(localStorage.getItem("data")) || [];
